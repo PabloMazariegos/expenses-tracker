fb62615b161a8d06145250fceab99a6fca15881a
PabloMazariegos <pmazariegospinto@gmail.com>
2024-10-03 23:19:23 -0600

    fix eslint warnings

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -2,25 +2,25 @@ import { createClient } from './server'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-  const supabase = createClient()
-  const userResponse = await supabase.auth.getUser()
+	const supabase = createClient()
+	const userResponse = await supabase.auth.getUser()
 
-  const pathName = request.nextUrl.pathname
+	const pathName = request.nextUrl.pathname
 
-  if(!userResponse.data.user && pathName.startsWith('/dashboard')){
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
+	if(!userResponse.data.user && pathName.startsWith('/dashboard')){
+		const url = request.nextUrl.clone()
+		url.pathname = '/login'
 
-    return NextResponse.redirect(url)
-  }
+		return NextResponse.redirect(url)
+	}
 
-  if (userResponse.data.user && pathName === '/login') {
-    const url = request.nextUrl.clone();
-    url.pathname = '/dashboard';
+	if (userResponse.data.user && pathName === '/login') {
+		const url = request.nextUrl.clone();
+		url.pathname = '/dashboard';
     
-    return NextResponse.redirect(url);
-  }
+		return NextResponse.redirect(url);
+	}
 
-  return NextResponse.next({ request })
+	return NextResponse.next({ request })
 
 }
