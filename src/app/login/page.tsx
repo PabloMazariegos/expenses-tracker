f963bef73f989ee25017362b9ac11629e6929f8c
PabloMazariegos <pablomazariegos15@gmail.com>
2024-10-16 20:57:15 -0600

    empty commit to test branch protection

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,7 +8,9 @@ import { signInWithGithub } from '@/utils/auth-providers/actions'
 export default function Login() {
 	return (
 		<div className='w-full flex flex-col items-center justify-center text-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
-			<p className='text-4xl md:text-5xl font-bold tracking-tighter'> Login </p>
+			<p className='text-4xl md:text-5xl font-bold tracking-tighter'> 
+				Login 
+			</p>
 			<Button  
 				startContent={<FaGithub/>}
 				onClick={signInWithGithub}
